967cc43b9d7f3acb574efccec0e8a36daadc6bc9
dsli208 <ds.li@outlook.com>
2019-10-07 03:10:21 -0400

    Modify loss function (nce)

--- CSE538_assignment_1_2019_Fall/Assignment1_for_students/loss_func.py
+++ CSE538_assignment_1_2019_Fall/Assignment1_for_students/loss_func.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 import sys
 import math
 
@@ -74,23 +75,56 @@ def nce_loss(inputs, weights, biases, labels, sample, unigram_prob):
 
     ==========================================================================
     """
+    # tf.enable_eager_execution()
+    print(inputs)
+    biases_shape = biases.shape.as_list()
+    print(biases_shape[0])
+    labels_shape = labels.shape.as_list()
+    print(labels_shape[0])
+    k = sample.size
 
-    u_dot = tf.multiply(tf.transport(inputs), labels)
+    # Get dot product and u values
+    u_dot = tf.multiply(tf.transpose(inputs), tf.cast(labels, tf.float32))
     u_o = tf.diag_part(u_dot)
     u_x = tf.reduce_sum(u_dot, 1, keep_dims=True)
 
+    # Values complimenting u_o
     b_o = tf.gather(biases, labels) # [biases[labels[i]] for i in range(len(labels))]
-    tf.reshape(b_o, [batch_size, 1])
+    tf.reshape(b_o, [labels_shape[0], 1])
     s_o = tf.add(u_o, b_o) # differences between biases and b_o?
 
-    k = samples.size
+    # Probability of all words w_o
+    unigram_probability_np = np.asarray(unigram_prob, np.float32)
+    unigram_prob_tf = tf.convert_to_tensor(unigram_probability_np, np.float32)
 
-    b_x = tf.gather(biases, samples)
+    unigram_prob_labels = tf.gather(unigram_prob_tf, labels)
+
+    # p_w_o = []
+    # for i in range(labels_shape[0]):
+        # p_w_o.append(k * unigram_prob[labels[i]])
+    # pre_sigma_o = s_o - math.log(k * unigram_prob[labels[i]])
+    # HOW TO MULTIPLY UNIGRAMPROBLABELS BY K?
+    pre_sigma_o = tf.subtract(s_o, tf.log(unigram_prob_labels))
+
+    # Values complimenting u_x
+    b_x = tf.gather(biases, sample)
     tf.reshape(b_x, [k, 1])
     s_x = tf.add(u_x, b_x)
 
+    # Probabilities of all words w_x
+    # p_w_x = []
+    # for i in range(len(samples)):
+        # p_w_x_.append(k * unigram_prob[samples[i]])
 
+    # Subtractions: s_o - p_w_o and s_x - p_w_x
+    pre_sigma_x = tf.subtract(s_x, tf.log(unigram_prob_labels))
+
+    # pre_sigma_x = s_x - math.log(k * unigram_prob[samples[i]]) for i in range(len(samples))
     # A = log(sigma(s_o) - log(num_sampled * P(w_o)))
     # B = sum(log(1 - C))
     # C = sigma(s_x) - log(num_sampled * P(w_x))
-    # sigma_matrix(x) =
+
+    p_sigma_o = tf.sigmoid(pre_sigma_o)
+    p_sigma_x = tf.sigmoid(pre_sigma_x)
+
+    return tf.subtract(D, E)
