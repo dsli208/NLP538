e8a5c1c25f5baa6f67a51da2444528ca617c85ec
dsli208 <ds.li@outlook.com>
2019-10-22 16:32:01 -0400

    Finish DAN

--- Assign2/Assign2/sequence_to_vector.py
+++ Assign2/Assign2/sequence_to_vector.py
@@ -95,8 +95,7 @@ class DanSequenceToVector(SequenceToVector):
              sequence_mask: tf.Tensor, # Boolean tensor of shape (batch_size, max_tokens_num). Entries with 1 indicate that token is a real token, and 0 indicate that it's a padding token.
              training=False) -> tf.Tensor:
         # TODO(students): start
-        # import pdb
-        # pdb.set_trace()
+
 
         # print("Vector Sequence")
         # print(vector_sequence)
@@ -108,14 +107,26 @@ class DanSequenceToVector(SequenceToVector):
             # Drop out - want to turn off some neurons in the NN
             # Some inputs we want to ignore
             # Which ones are 0's in the matrix and which ones are 1's?
-            uni_mask = tf.random.uniform(tf.shape(vector_sequence), 0, 1, float64, self.dropout)
-            bernoulli_mask = tf.map_fn(lambda x: tf.cond(x >= self.dropout, lambda x: 1, lambda x: 0), uni_mask)
-            pass
+
+            a = tf.constant(1, shape=vector_sequence.shape, dtype=tf.float32)
+            b = tf.constant(0, shape=vector_sequence.shape, dtype=tf.float32)
+
+            # import pdb
+            # pdb.set_trace()
+            uni_mask = tf.Variable(tf.random.uniform(tf.shape(vector_sequence), minval=0, maxval=1, dtype=tf.float32, seed=self.dropout))
+            bernoulli_mask = tf.where(tf.greater_equal(uni_mask, self.dropout), a, b)
+            # bernoulli_mask = tf.map_fn(lambda x: tf.cond(x >= self.dropout, lambda x: 1, lambda x: 0), uni_mask)
+
+            # tf.print(uni_mask)
+            # tf.print(bernoulli_mask)
+            # bernoulli_mask = tf.reshape(bernoulli_mask, vector_sequence)
+
+            inputs = tf.multiply(vector_sequence, bernoulli_mask)
         else:
             inputs = vector_sequence
 
         # Get average
-        h = tf.reduce_mean(vector_sequence, axis=1) # avg
+        h = tf.reduce_mean(inputs, axis=1) # avg
 
         for d in self.dense_layers:
             h = d(h)
@@ -151,11 +162,13 @@ class GruSequenceToVector(SequenceToVector):
     """
     def __init__(self, input_dim: int, num_layers: int):
         super(GruSequenceToVector, self).__init__(input_dim)
+
+        # TODO(students): start
         self.num_layers = num_layers
 
-        self.gru = tf.keras.layers.GRU(input_dim, activation='tanh')
-        # TODO(students): start
-        # ...
+        self.gru_layers = []
+        for i in range(0, num_layers):
+            gru_layers.append(tf.keras.layers.GRU(input_dim, activation='tanh', return_state=True, return_sequences=True))
         # TODO(students): end
 
     def call(self,
@@ -163,6 +176,15 @@ class GruSequenceToVector(SequenceToVector):
              sequence_mask: tf.Tensor,
              training=False) -> tf.Tensor:
         # TODO(students): start
+        if training:
+            sequence_mask_2 = tf.reshape(sequence_mask, tf.shape(vector_sequence))
+            inputs = tf.multiply(vector_sequence, sequence_mask)
+        else:
+            inputs = vector_sequence
+
+        h = inputs
+        for g in self.gru_layers:
+            h = g(h)
 
         # TODO(students): end
         return {"combined_vector": combined_vector,
